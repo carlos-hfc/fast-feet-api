c9aa09a5f87d256a593fb23cdb62fd266246b471
Carlos Faustino <chfcchfc96@gmail.com>
2024-05-24 15:18:16 -0300

    feat: add register admin use case

--- src/domain/delivery/application/repositories/admins-repository.ts
+++ src/domain/delivery/application/repositories/admins-repository.ts
@@ -2,6 +2,7 @@ import { Admin } from "../../enterprise/entities/admin"
 
 export abstract class AdminsRepository {
   abstract findByCpf(cpf: string): Promise<Admin | null>
+  abstract findByEmail(email: string): Promise<Admin | null>
   abstract save(admin: Admin): Promise<void>
   abstract create(admin: Admin): Promise<void>
 }

dd54a042bdc23b00929d21e6165340ddf32fac53
Carlos Faustino <chfcchfc96@gmail.com>
2024-05-24 15:17:29 -0300

    feat: updare core error

--- src/core/errors/student-already-exists.ts
+++
@@ -1,7 +0,0 @@
-import { UseCaseError } from "./use-case-error"
-
-export class StudentAlreadyExists extends Error implements UseCaseError {
-  constructor(identifier: string) {
-    super(`Student "${identifier}" already exists.`)
-  }
-}

dc8b61fd46e2a81d3345953b4276e5cd98aaeb36
Carlos Faustino <chfcchfc96@gmail.com>
2024-06-04 12:59:24 -0300

    feat: update setup e2e tests

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -4,37 +4,39 @@ import { randomUUID } from "node:crypto"
 import { PrismaClient } from "@prisma/client"
 import { config } from "dotenv"
 
+import { envSchema } from "@/infra/env/env"
+
 config({ path: ".env", override: true })
 config({ path: ".env.test", override: true })
 
+const env = envSchema.parse(process.env)
+
 const prisma = new PrismaClient()
 
-function generateDbUrl(databaseName: string) {
-  if (!process.env.DATABASE_URL) {
+function generateDbUrl(schemaId: string) {
+  if (!env.DATABASE_URL) {
     throw new Error("Please provide a DATABASE_URL environment variable")
   }
 
-  const url = new URL(process.env.DATABASE_URL)
+  const url = new URL(env.DATABASE_URL)
 
-  url.pathname = `/${databaseName}`
+  url.searchParams.set("schema", schemaId)
 
   return url.toString()
 }
 
 const schema = randomUUID()
 
-const dbName = `nest_clean_${schema.replace(/-/g, "_")}`
-
 beforeAll(async () => {
-  const dbUrl = generateDbUrl(dbName)
+  const dbUrl = generateDbUrl(schema)
 
   process.env.DATABASE_URL = dbUrl
 
-  execSync("pnpm prisma migrate deploy")
+  execSync("npm prisma migrate deploy")
 })
 
 afterAll(async () => {
-  await prisma.$executeRawUnsafe(`DROP DATABASE IF EXISTS ${dbName};`)
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE;`)
 
   await prisma.$disconnect()
 })

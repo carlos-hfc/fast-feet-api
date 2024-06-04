cd02d64d20743aae35da86742072d36ddaa927df
Carlos Faustino <chfcchfc96@gmail.com>
2024-06-04 15:27:43 -0300

    feat: add delete deliveryman controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common"
 
 import { AuthenticateAdminUseCase } from "@/domain/delivery/application/use-cases/authenticate-admin"
 import { CreateDeliverymanUseCase } from "@/domain/delivery/application/use-cases/create-deliveryman"
+import { DeleteDeliverymanUseCase } from "@/domain/delivery/application/use-cases/delete-deliveryman"
 import { EditDeliverymanUseCase } from "@/domain/delivery/application/use-cases/edit-deliveryman"
 import { RegisterAdminUseCase } from "@/domain/delivery/application/use-cases/register-admin"
 
@@ -9,6 +10,7 @@ import { CryptographyModule } from "../cryptography/cryptography.module"
 import { DatabaseModule } from "../database/database.module"
 import { AuthenticateAdminController } from "./controllers/authenticate-admin.controller"
 import { CreateDeliverymanController } from "./controllers/create-deliveryman.controller"
+import { DeleteDeliverymanController } from "./controllers/delete-deliveryman.controller"
 import { EditDeliverymanController } from "./controllers/edit-deliveryman.controller"
 import { RegisterAdminController } from "./controllers/register-admin.controller"
 
@@ -19,12 +21,14 @@ import { RegisterAdminController } from "./controllers/register-admin.controller
     AuthenticateAdminController,
     CreateDeliverymanController,
     EditDeliverymanController,
+    DeleteDeliverymanController,
   ],
   providers: [
     RegisterAdminUseCase,
     AuthenticateAdminUseCase,
     CreateDeliverymanUseCase,
     EditDeliverymanUseCase,
+    DeleteDeliverymanUseCase,
   ],
 })
 export class HttpModule {}

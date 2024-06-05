8987fe209034e50c11ae222870c447d409e84435
Carlos Faustino <chfcchfc96@gmail.com>
2024-06-05 10:32:39 -0300

    feat: add storage module

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -5,6 +5,10 @@ export const envSchema = z.object({
   NODE_ENV: z.enum(["dev", "test"]).default("dev"),
   PORT: z.coerce.number().default(3333),
   JWT_SECRET: z.string(),
+  CLOUDFLARE_ACCOUNT_ID: z.string(),
+  AWS_BUCKET_NAME: z.string(),
+  AWS_ACCESS_KEY_ID: z.string(),
+  AWS_SECRET_ACCESS_KEY: z.string(),
 })
 
 export type Env = z.infer<typeof envSchema>
